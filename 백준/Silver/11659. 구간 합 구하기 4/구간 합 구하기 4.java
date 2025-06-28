8870564218b61a307cd7d30e8b7c2cea23f5a3d3
hyuckjoon9 <144460238+hyuckjoon9@users.noreply.github.com>
2025-06-28 14:43:04 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1872 ms, Memory: 257224 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -5,18 +5,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
-        int[] sumArr = new int[N];
-        for (int i = 0; i < N; i++) {
-            if (i == 0) sumArr[i] = sc.nextInt();
-            else sumArr[i] = sumArr[i - 1] + sc.nextInt();
+        int[] sumArr = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            sumArr[i] = sumArr[i - 1] + sc.nextInt();
         }
 
         int result = 0;
         for (int i = 0; i < M; i++) {
-            int startIdx = sc.nextInt() - 1;
-            int endIdx = sc.nextInt() - 1;
-            if (startIdx == 0) result = sumArr[endIdx];
-            else result = sumArr[endIdx] - sumArr[startIdx - 1];
+            int startIdx = sc.nextInt();
+            int endIdx = sc.nextInt();
+            result = sumArr[endIdx] - sumArr[startIdx - 1];
             System.out.println(result);
         }
     }
