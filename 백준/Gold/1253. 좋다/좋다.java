95809da45becd11539ea8f5cd608a24e4aa197a5
hyuckjoon9 <144460238+hyuckjoon9@users.noreply.github.com>
2025-08-28 13:03:51 +0900

    [Gold IV] Title: 좋다, Time: 144 ms, Memory: 14720 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,40 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        long[] arr = new long[n];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int arr[] = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
 
-        int answer = 0;
+        int count = 0;
+
         for (int i = 0; i < n; i++) {
             int startIdx = 0;
             int endIdx = n - 1;
-            int find = (int) arr[i];
             while (startIdx < endIdx) {
-                if (startIdx == i) {
-                    startIdx++;
-                    continue;
-                } else if (endIdx == i) {
-                    endIdx--;
-                    continue;
-                }
-                int sum = (int) (arr[startIdx] + arr[endIdx]);
-                if (sum > find) endIdx--;
-                else if (sum < find) startIdx++;
+                int sum = arr[startIdx] + arr[endIdx];
+                if (sum < arr[i]) startIdx++;
+                else if (sum > arr[i]) endIdx--;
                 else {
-                    answer++;
-                    break;
+                    if (startIdx != i && endIdx != i) {
+                        count++;
+                        break;
+                    } else if (startIdx == i) startIdx++;
+                    else if (endIdx == i) endIdx--;
                 }
             }
         }
-        sc.close();
-        System.out.println(answer);
+        System.out.println(count);
     }
 }
