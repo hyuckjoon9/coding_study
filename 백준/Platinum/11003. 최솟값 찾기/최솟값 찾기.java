c3defb84f2cbcaa4173ee1c2f781d89ae1301bb6
hyuckjoon9 <144460238+hyuckjoon9@users.noreply.github.com>
2025-09-01 17:00:13 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 1976 ms, Memory: 642028 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,44 +1,11 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
-    static Deque<Node> dq;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int L = Integer.parseInt(st.nextToken());
-        dq = new ArrayDeque<>();
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            int now = Integer.parseInt(st.nextToken());
-            if (dq.isEmpty()) {
-                dq.addLast(new Node(i, now));
-            } else if (i - dq.getFirst().idx < L && dq.size() < L) {
-                comp(now, i);
-            } else {
-                dq.removeFirst();
-                comp(now, i);
-            }
-            bw.write(dq.getFirst().val + " ");
-        }
-        bw.flush();
-        bw.close();
-    }
-
-    public static void comp(int now, int i) {
-        while (!dq.isEmpty() && dq.getLast().val > now) {
-            dq.removeLast();
-        }
-        dq.addLast(new Node(i, now));
-    }
-
     static class Node {
         int idx;
         int val;
@@ -47,5 +14,32 @@ public class Main {
             this.idx = idx;
             this.val = val;
         }
+    }
+
+    static Deque<Node> dq;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1 << 16);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        dq = new ArrayDeque<>();
+
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int now = Integer.parseInt(st.nextToken());
+            while (!dq.isEmpty() && dq.getLast().val > now) {
+                dq.removeLast();
+            }
+
+            dq.addLast(new Node(i, now));
+
+            if (dq.getFirst().idx <= i - L) dq.removeFirst();
+
+            sb.append(dq.getFirst().val).append(' ');
+        }
+        System.out.println(sb.toString());
     }
 }
