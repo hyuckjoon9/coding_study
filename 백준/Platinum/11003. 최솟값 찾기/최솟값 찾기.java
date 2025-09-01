49277376494c39880ad9e2f746113c4c82caf620
hyuckjoon9 <144460238+hyuckjoon9@users.noreply.github.com>
2025-09-01 16:30:49 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2060 ms, Memory: 529696 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,50 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
+    static Deque<Node> dq;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int l = Integer.parseInt(st.nextToken());
-
-        StringBuilder sb = new StringBuilder();
-        Deque<Node> dq = new LinkedList<>();
+        int L = Integer.parseInt(st.nextToken());
+        dq = new ArrayDeque<>();
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            int cur = Integer.parseInt(st.nextToken());
-            if (dq.isEmpty()) dq.addLast(new Node(i, cur));
-            else {
-                // 슬라이딩 윈도우 범위가 L을 넘어갈 때
-                if (i - dq.getFirst().index >= l) {
-                    dq.removeFirst();
-                }
-
-                // 마지막 value가 cur보다 클 때
-                while (!dq.isEmpty() && dq.getLast().value > cur) {
-                    dq.removeLast();
-                }
-
-                dq.addLast(new Node(i, cur));
+            int now = Integer.parseInt(st.nextToken());
+            if (dq.isEmpty()) {
+                dq.addLast(new Node(i, now));
+            } else if (i - dq.getFirst().idx < L && dq.size() < L) {
+                comp(now, i);
+            } else {
+                dq.removeFirst();
+                comp(now, i);
             }
-
-            sb.append(dq.getFirst().value).append(' ');
+            bw.write(dq.getFirst().val + " ");
         }
-        System.out.println(sb);
+        bw.flush();
+        bw.close();
+    }
+
+    public static void comp(int now, int i) {
+        while (!dq.isEmpty() && dq.getLast().val > now) {
+            dq.removeLast();
+        }
+        dq.addLast(new Node(i, now));
     }
 
     static class Node {
-        int index;
-        int value;
+        int idx;
+        int val;
 
-        Node(int index, int value) {
-            this.index = index;
-            this.value = value;
+        Node(int idx, int val) {
+            this.idx = idx;
+            this.val = val;
         }
     }
 }
