00d9cf697aa6605c3dbaaa3546f27159530fa560
hyuckjoon9 <144460238+hyuckjoon9@users.noreply.github.com>
2025-09-03 16:39:27 +0900

    [Silver I] Title: 절댓값 힙, Time: 284 ms, Memory: 24676 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,26 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        PriorityQueue<Integer> p = new PriorityQueue<>((a, b) -> {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
             int firstAbs = Math.abs(a);
             int secondAbs = Math.abs(b);
+
             if (firstAbs == secondAbs) return a > b ? 1 : -1;
             else return firstAbs - secondAbs;
-
         });
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        StringBuilder sb = new StringBuilder();
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            int num = sc.nextInt();
-            if (num == 0) {
-                sb.append(p.isEmpty() ? "0\n" : p.poll() + "\n");
-                continue;
-            }
-            p.add(num);
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+            else pq.add(x);
         }
         System.out.println(sb);
     }
