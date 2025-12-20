ae15c3537733a0ca162ccfe1a51084f6f0375545
hyuckjoon9 <144460238+hyuckjoon9@users.noreply.github.com>
2025-12-20 21:23:25 +0900

    [Silver III] Title: 소수 구하기, Time: 272 ms, Memory: 22808 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,22 +11,25 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
 
+        boolean[] isPrime = new boolean[N + 1];
+        Arrays.fill(isPrime, true);
+
+        isPrime[0] = false;
+        isPrime[1] = false;
+
         StringBuilder sb = new StringBuilder();
+        for (int i = 2; i * i <= N; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
         for (int i = M; i <= N; i++) {
-            if (isPrime(i)) sb.append(i).append("\n");
+            if (isPrime[i]) sb.append(i).append("\n");
         }
         System.out.println(sb);
 
-    }
-
-    public static boolean isPrime(int x) {
-        if (x == 2) return true;
-        if (x < 2) return false;
-        if (x % 2 == 0) return false;
-
-        for (int i = 3; i * i <= x; i += 2) {
-            if (x % i == 0) return false;
-        }
-        return true;
     }
 }
