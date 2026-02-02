a8e0e77b0bd183ce6f589652ddb1b57bbc73cb46
hyuckjoon9 <144460238+hyuckjoon9@users.noreply.github.com>
2026-02-02 16:10:29 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 172 ms, Memory: 16460 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -1,49 +1,51 @@
+/*
+ * 단어는 공백으로 구분
+ * 만약 태그가 있다면 태그를 제외한 문자열만 뒤집기
+ * 1. '<' 를 만나면 '>'가 나올때까지 sb에 넣기
+ * 2. 단어를 만나면 공백을 만나거나 '<'를 만나거나 끝나거나.
+ * Character List인 onlyWord에 공백, '<', 끝나기 전까지 넣기
+ * 3. 공백, '<' 을 만났다면 onlyWord에 있는거 뒤집어서 sb에 넣기
+ * */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String initStr = br.readLine();
+        String s = br.readLine();
 
-        Deque<Character> onlyWord = new ArrayDeque<>();
+        StringBuilder result = new StringBuilder();
+        StringBuilder word = new StringBuilder();
         boolean isTag = false;
-        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < initStr.length(); i++) {
-            if (initStr.charAt(i) == '<') {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '<') {
+                result.append(word.reverse());
+                word.setLength(0);
                 isTag = true;
-                while (!onlyWord.isEmpty()) {
-                    char c = onlyWord.removeLast();
-                    sb.append(c);
+                result.append(c);
+            } else if (c == '>') {
+                result.append(c);
+                isTag = false;
+            } else if (isTag) {
+                result.append(c);
+            } else {
+                if (c == ' ') {
+                    result.append(word.reverse().append(' '));
+                    word.setLength(0);
+                } else {
+                    word.append(c);
                 }
-            } else if (initStr.charAt(i) == '>') isTag = false;
-
-            if (isTag || initStr.charAt(i) == '>') {
-                sb.append(initStr.charAt(i));
-                continue;
             }
-
-            if (initStr.charAt(i) != ' ' && initStr.charAt(i) != '<') {
-                onlyWord.addLast(initStr.charAt(i));
-                continue;
-            }
-            while (!onlyWord.isEmpty()) {
-                char c = onlyWord.removeLast();
-                sb.append(c);
-            }
-
-            sb.append(" ");
         }
 
-        while (!onlyWord.isEmpty()) {
-            char c = onlyWord.removeLast();
-            sb.append(c);
-        }
+        result.append(word.reverse());
 
-        System.out.println(sb);
+        System.out.println(result);
+
     }
 }
