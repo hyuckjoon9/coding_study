4fcc7aa9d8583f5b64d56bb0ea141c3f3667958f
hyuckjoon9 <144460238+hyuckjoon9@users.noreply.github.com>
2026-04-20 13:33:50 +0900

    [Silver III] Title: N과 M (1), Time: 256 ms, Memory: 22844 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,37 +1,56 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-    static int n;
-    static int m;
-    static int[] result;
-    static boolean[] visited;
+class Main {
+	static int M;
+	static int N;
+	static int[] arr;
+	static int[] answer;
+	static StringBuilder sb;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+	static boolean[] visited;
 
-        result = new int[m];
-        visited = new boolean[n + 1];
-        tracking(0);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    static void tracking(int len) {
-        if (len == m) {
-            for (int i = 0; i < m; i++) {
-                System.out.print(result[i] + " ");
-            }
-            System.out.println();
-            return;
-        }
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                result[len] = i;
-                tracking(len + 1);
-                visited[i] = false;
-            }
-        }
-    }
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = i + 1;
+		}
+
+		answer = new int[M];
+		visited = new boolean[N];
+		sb = new StringBuilder();
+
+		permutation(0);
+
+		System.out.println(sb);
+	}
+
+	public static void permutation(int depth) {
+		if (depth == M) {
+			for (int v : answer) {
+				sb.append(v).append(" ");
+
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (visited[i])
+				continue;
+			visited[i] = true;
+			answer[depth] = arr[i];
+
+			permutation(depth + 1);
+			visited[i] = false;
+		}
+	}
 }
