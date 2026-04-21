1504cce6c32690de5b36b827bd0ab3af7ec72b72
hyuckjoon9 <144460238+hyuckjoon9@users.noreply.github.com>
2026-04-21 13:21:30 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 445 ms, Memory: 12,540 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.cpp
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.cpp
@@ -1,13 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static List<int[]> cust;
-	static int[][] permutes;
+	static int[][] customers;
 	static int N;
 	static int minDist;
 	static int companyX;
@@ -24,7 +21,7 @@ public class Solution {
 
 		for (int test_case = 1; test_case <= T; test_case++) {
 			N = Integer.parseInt(br.readLine());
-			permutes = new int[N][2];
+			customers = new int[N][2];
 			minDist = Integer.MAX_VALUE;
 
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -35,14 +32,15 @@ public class Solution {
 			homeX = Integer.parseInt(st.nextToken());
 			homeY = Integer.parseInt(st.nextToken());
 
-			cust = new ArrayList();
 			for (int i = 0; i < N; i++) {
 				int x = Integer.parseInt(st.nextToken());
 				int y = Integer.parseInt(st.nextToken());
-				cust.add(new int[] { x, y });
+
+				customers[i][0] = x;
+				customers[i][1] = y;
 			}
 
-			permutation(0, 0);
+			dfs(0, 0, companyX, companyY, 0);
 
 			sb.append("#").append(test_case).append(" ").append(minDist).append("\n");
 
@@ -51,27 +49,26 @@ public class Solution {
 		System.out.println(sb);
 	}
 
-	public static void permutation(int depth, int flag) {
-		if (depth == N) {
-			int dist = 0;
-			dist += Math.abs(companyX - permutes[0][0]) + Math.abs(companyY - permutes[0][1]);
-			dist += Math.abs(homeX - permutes[permutes.length - 1][0])
-					+ Math.abs(homeY - permutes[permutes.length - 1][1]);
-			for (int i = 0; i < permutes.length - 1; i++) {
-				int distX = Math.abs(permutes[i][0] - permutes[i + 1][0]);
-				int distY = Math.abs(permutes[i][1] - permutes[i + 1][1]);
-				dist += distX + distY;
-			}
-			minDist = Math.min(dist, minDist);
+	public static void dfs(int depth, int visited, int curX, int curY, int dist) {
+		if (dist >= minDist)
+			return;
 
+		if (depth == N) {
+			dist += Math.abs(homeX - curX) + Math.abs(homeY - curY);
+			minDist = Math.min(minDist, dist);
 			return;
 		}
 
 		for (int i = 0; i < N; i++) {
-			if ((flag & (1 << i)) != 0)
+			if ((visited & (1 << i)) != 0)
 				continue;
-			permutes[depth] = cust.get(i);
-			permutation(depth + 1, flag | (1 << i));
+
+			int nextX = customers[i][0];
+			int nextY = customers[i][1];
+
+			int nextDist = Math.abs(customers[i][0] - curX) + Math.abs(customers[i][1] - curY) + dist;
+
+			dfs(depth + 1, visited | (1 << i), nextX, nextY, nextDist);
 		}
 	}
 
