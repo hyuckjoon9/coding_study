2617b7be007afb09b4ad1df84a199282397b0763
hyuckjoon9 <144460238+hyuckjoon9@users.noreply.github.com>
2026-05-29 18:08:36 +0900

    [level 3] Title: 단어 변환, Time: 8.24 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,74 +2,52 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 class Solution {
-	String begin;
-	String target;
-	String[] words;
-	int answer;
-	Deque<Node> dq;
-	boolean[] visited;
-
 	class Node {
 		String word;
-		int step;
+		int dist;
 
-		Node(String word, int step) {
+		Node(String word, int dist) {
 			this.word = word;
-			this.step = step;
+			this.dist = dist;
 		}
 	}
 
 	public int solution(String begin, String target, String[] words) {
-		this.begin = begin;
-		this.target = target;
-		this.words = words;
-		dq = new ArrayDeque<>();
-		visited = new boolean[words.length];
+		boolean[] visited = new boolean[words.length];
 
-		for (int i = 0; i < words.length; i++) {
-			int diffCnt = getDiff(begin, words[i]);
-			if (diffCnt == 1) {
-				dq.addLast(new Node(words[i], 1));
-				visited[i] = true;
-			}
-		}
+		Deque<Node> dq = new ArrayDeque<>();
+		dq.addLast(new Node(begin, 0));
 
-		if (dq.isEmpty())
-			return 0;
-		answer = 0;
-		check();
-		return answer;
-	}
-
-	public void check() {
 		while (!dq.isEmpty()) {
 			Node cur = dq.removeFirst();
-
-			if (cur.word.equals(target)) {
-				answer = cur.step;
-			}
+			System.out.println("word: " + cur.word + " dist: " + cur.dist);
 
 			for (int i = 0; i < words.length; i++) {
 				if (visited[i])
 					continue;
-				int diffCnt = getDiff(words[i], cur.word);
-				if (diffCnt == 1) {
-					visited[i] = true;
-					dq.addLast(new Node(words[i], cur.step + 1));
-				}
-			}
+				if (!isDiffOne(cur.word, words[i]))
+					continue;
 
+				if (words[i].equals(target)) {
+					return cur.dist + 1;
+				}
+				visited[i] = true;
+				dq.addLast(new Node(words[i], cur.dist + 1));
+			}
 		}
+
+		return 0;
 	}
 
-	public int getDiff(String a, String b) {
-		int diffCnt = 0;
+	public boolean isDiffOne(String a, String b) {
+		int len = a.length();
+		int diff = 0;
 
-		for (int i = 0; i < a.length(); i++) {
+		for (int i = 0; i < len; i++) {
 			if (a.charAt(i) != b.charAt(i))
-				diffCnt++;
+				diff++;
 		}
 
-		return diffCnt;
+		return diff == 1;
 	}
 }
