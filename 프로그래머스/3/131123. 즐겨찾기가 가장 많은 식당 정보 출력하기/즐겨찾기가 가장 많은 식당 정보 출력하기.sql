9e3682b591a7cb0602a00e21736049dcb96f1183
hyuckjoon9 <144460238+hyuckjoon9@users.noreply.github.com>
2026-06-02 11:27:46 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,20 @@
+select
+    a.FOOD_TYPE,
+    a.REST_ID,
+    a.REST_NAME,
+    a.FAVORITES
+from
+    REST_INFO a
+join
+    (select
+        FOOD_TYPE,
+        max(FAVORITES) as `FAVORITES`
+    from
+        REST_INFO
+    group by
+        FOOD_TYPE) b
+    on a.FOOD_TYPE = b.FOOD_TYPE
+    and a.FAVORITES=b.FAVORITES
+order by
+    FOOD_TYPE desc
+;
