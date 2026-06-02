e43ff4a536045b1d8ca6bedd34cad2c8ff923ad8
hyuckjoon9 <144460238+hyuckjoon9@users.noreply.github.com>
2026-06-02 09:11:16 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,53 +1,25 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 class Solution {
 	public int solution(int[] arr) {
-		int answer = 0;
+		int answer = arr[0];
 
-		Deque<Integer> dq = new ArrayDeque<Integer>();
-
-		for (int i = 0; i < arr.length; i += 2) {
-			if (i == arr.length - 1) {
-				dq.addLast(arr[i]);
-				continue;
-			}
-
-			dq.addLast(lcm(arr[i], arr[i + 1]));
+		for (int i = 1; i < arr.length; i++) {
+			answer = lcm(answer, arr[i]);
 		}
 
-		while (dq.size() != 1) {
-			int len = dq.size();
-			for (int i = 0; i < len; i += 2) {
-				if (i == len - 1) {
-					dq.addLast(dq.removeFirst());
-					continue;
-				}
-
-				int n1 = dq.removeFirst();
-				int n2 = dq.removeFirst();
-
-				dq.addLast(lcm(n1, n2));
-			}
-		}
-
-		return dq.removeFirst();
-	}
-
-	public int lcm(int a, int b) {
-		int lcmValue = a / gcd(a, b) * b;
-
-		return lcmValue;
-
+		return answer;
 	}
 
 	public int gcd(int a, int b) {
 		while (b != 0) {
-			int temp = a % b;
-			a = b;
-			b = temp;
+			int temp = b;
+			b = a % b;
+			a = temp;
 		}
 
 		return a;
+	}
+
+	public int lcm(int a, int b) {
+		return a / gcd(a, b) * b;
 	}
 }
