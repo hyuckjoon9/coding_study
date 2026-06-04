fdf578804124a14fdc49ac2d7823af60af0ae402
hyuckjoon9 <144460238+hyuckjoon9@users.noreply.github.com>
2026-06-04 13:29:23 +0900

    [level 3] Title: 여행경로, Time: 0.56 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -3,16 +3,16 @@ import java.util.Arrays;
 import java.util.List;
 
 class Solution {
-	boolean[] visited;
-	List<String> result;
+	List<String> path;
 	String[][] tickets;
-	int count;
 	String[] answer;
-	boolean found;
+	boolean[] visited;
+	boolean end;
 
 	public String[] solution(String[][] tickets) {
 		this.tickets = tickets;
-		count = 0;
+		path = new ArrayList<>();
+		visited = new boolean[tickets.length];
 
 		Arrays.sort(tickets, (a, b) -> {
 			if (a[0].equals(b[0])) {
@@ -21,41 +21,30 @@ class Solution {
 			return a[0].compareTo(b[0]);
 		});
 
-		visited = new boolean[tickets.length];
-		result = new ArrayList<>();
+		path.add("ICN");
+		track("ICN", 0);
 
-		result.add("ICN");
-		back("ICN", 0);
-
-		for (String s : answer) {
-			System.out.print(s + " ");
-		}
 		return answer;
 	}
 
-	public void back(String cur, int count) {
-		if (found)
-			return;
-
-		if (count == tickets.length) {
-			answer = result.toArray(new String[0]);
-			found = true;
+	public void track(String start, int cnt) {
+		if (cnt == tickets.length) {
+			answer = path.toArray(new String[0]);
+			end = true;
 			return;
 		}
-
 		for (int i = 0; i < tickets.length; i++) {
-			if (visited[i])
+			if (visited[i] || !start.equals(tickets[i][0]))
 				continue;
-			if (!tickets[i][0].equals(cur))
-				continue;
-
+			if (end)
+				return;
 			visited[i] = true;
-			result.add(tickets[i][1]);
+			path.add(tickets[i][1]);
 
-			back(tickets[i][1], count + 1);
+			track(tickets[i][1], cnt + 1);
 
 			visited[i] = false;
-			result.remove(result.size() - 1);
+			path.remove(path.size() - 1);
 		}
 	}
 }
