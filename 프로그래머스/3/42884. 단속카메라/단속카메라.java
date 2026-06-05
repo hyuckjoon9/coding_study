a215a86a5571cc0a2c313bff657bc280d2ca2d88
hyuckjoon9 <144460238+hyuckjoon9@users.noreply.github.com>
2026-06-05 13:30:35 +0900

    [level 3] Title: 단속카메라, Time: 5.79 ms, Memory: 59.9 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,18 +2,20 @@ import java.util.Arrays;
 
 class Solution {
 	public int solution(int[][] routes) {
-		Arrays.sort(routes, (a, b) -> Integer.compare(a[1], b[1]));
+		Arrays.sort(routes, (a, b) -> {
+			return Integer.compare(a[1], b[1]);
+		});
 
 		int camera = routes[0][1];
-		int cameraCnt = 1;
+		int answer = 1;
 
 		for (int i = 0; i < routes.length; i++) {
-			if (routes[i][0] <= camera && camera <= routes[i][1])
-				continue;
-			camera = routes[i][1];
-			cameraCnt++;
+			if (routes[i][0] > camera) {
+				answer++;
+				camera = routes[i][1];
+			}
 		}
 
-		return cameraCnt;
+		return answer;
 	}
 }
