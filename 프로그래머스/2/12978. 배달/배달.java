200cf06081a877d5179db3f1bf5d659419b061bc
hyuckjoon9 <144460238+hyuckjoon9@users.noreply.github.com>
2026-06-05 13:26:38 +0900

    [level 2] Title: 배달, Time: 3.37 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,14 +1,9 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 
 class Solution {
-	int N;
-	int K;
-	int[][] road;
-	List<Node>[] graph;
-	int[] dist;
-
 	class Node {
 		int to;
 		int weight;
@@ -21,54 +16,50 @@ class Solution {
 
 	public int solution(int N, int[][] road, int K) {
 		int answer = 0;
-		this.N = N;
-		this.K = K;
-		this.road = road;
-		graph = new ArrayList[N + 1];
-		dist = new int[N + 1];
+		List<Node>[] g = new ArrayList[N + 1];
 
 		for (int i = 0; i <= N; i++) {
-			graph[i] = new ArrayList<>();
-			dist[i] = Integer.MAX_VALUE;
+			g[i] = new ArrayList<>();
 		}
 
 		for (int i = 0; i < road.length; i++) {
 			int from = road[i][0];
 			int to = road[i][1];
 			int weight = road[i][2];
-
-			graph[from].add(new Node(to, weight));
-			graph[to].add(new Node(from, weight));
+			g[from].add(new Node(to, weight));
+			g[to].add(new Node(from, weight));
 		}
+
+		PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> {
+			return Integer.compare(a.weight, b.weight);
+		});
+
+		int[] dist = new int[N + 1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
 		dist[1] = 0;
-		dijkstra(1);
+
+		pq.add(new Node(1, 0));
+
+		while (!pq.isEmpty()) {
+			Node cur = pq.poll();
+
+			if (dist[cur.to] < cur.weight)
+				continue;
+
+			for (Node next : g[cur.to]) {
+				int nextWeight = dist[cur.to] + next.weight;
+
+				if (nextWeight < dist[next.to]) {
+					dist[next.to] = nextWeight;
+					pq.add(new Node(next.to, nextWeight));
+				}
+			}
+		}
 
 		for (int d : dist) {
 			if (d <= K)
 				answer++;
 		}
-
 		return answer;
-	}
-
-	public void dijkstra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.weight, b.weight));
-		pq.add(new Node(start, 0));
-
-		while (!pq.isEmpty()) {
-			Node cur = pq.poll();
-
-			if (cur.weight > dist[cur.to])
-				continue;
-
-			for (Node next : graph[cur.to]) {
-				int nextDist = dist[cur.to] + next.weight;
-
-				if (nextDist < dist[next.to]) {
-					dist[next.to] = nextDist;
-					pq.add(new Node(next.to, nextDist));
-				}
-			}
-		}
 	}
 }
